a179caeb6428b25a71675181ecf78c9f58a30186
Elliop <abdelmouhayminegx@gmail.com>
2021-12-28 18:05:50 +0100

    Add Like fct and other icons

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,8 +1,81 @@
-import { DotsHorizontalIcon } from "@heroicons/react/outline";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  setDoc,
+} from "@firebase/firestore";
+import {
+  ChartBarIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  HeartIcon,
+  ShareIcon,
+  SwitchHorizontalIcon,
+  TrashIcon,
+} from "@heroicons/react/outline";
+import {
+  HeartIcon as HeartIconFilled,
+  ChatIcon as ChatIconFilled,
+} from "@heroicons/react/solid";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Moment from "react-moment";
+import { db } from "../firebase";
 
-const Post = ({ id, post, postPage }) => {
+function Post({ id, post, postPage }) {
+  const { data: session } = useSession();
+  const [comments, setComments] = useState([]);
+  const [likes, setLikes] = useState([]);
+  const [liked, setLiked] = useState(false);
+  const router = useRouter();
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, "posts", id, "comments"),
+          orderBy("timestamp", "desc")
+        ),
+        (snapshot) => setComments(snapshot.docs)
+      ),
+    [db, id]
+  );
+
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "posts", id, "likes"), (snapshot) =>
+        setLikes(snapshot.docs)
+      ),
+    [db, id]
+  );
+
+  useEffect(
+    () =>
+      setLiked(
+        likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+      ),
+    [likes]
+  );
+
+  const likePost = async () => {
+    if (liked) {
+      await deleteDoc(doc(db, "posts", id, "likes", session.user.uid));
+    } else {
+      await setDoc(doc(db, "posts", id, "likes", session.user.uid), {
+        username: session.user.name,
+      });
+    }
+  };
+
   return (
-    <div className="p-3 flex cursor-pointer border-b border-gray-700">
+    <div
+      className="p-3 flex cursor-pointer border-b border-gray-700"
+      onClick={() => router.push(`/${id}`)}
+    >
       {!postPage && (
         <img
           src={post?.userImg}
@@ -33,10 +106,10 @@ const Post = ({ id, post, postPage }) => {
               >
                 @{post?.tag}
               </span>
-            </div>{" "}
+            </div>
             ·{" "}
             <span className="hover:underline text-sm sm:text-[15px]">
-              {/* <Moment fromNow>{post?.timestamp?.toDate()}</Moment> */}
+              <Moment fromNow>{post?.timestamp?.toDate()}</Moment>
             </span>
             {!postPage && (
               <p className="text-[#d9d9d9] text-[15px] sm:text-base mt-0.5">
@@ -56,9 +129,83 @@ const Post = ({ id, post, postPage }) => {
           alt=""
           className="rounded-2xl max-h-[700px] object-cover mr-2"
         />
+        <div
+          className={`text-[#6e767d] flex justify-between w-10/12 ${
+            postPage && "mx-auto"
+          }`}
+        >
+          <div
+            className="flex items-center space-x-1 group"
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <div className="icon group-hover:bg-[#1d9bf0] group-hover:bg-opacity-10">
+              <ChatIcon className="h-5 group-hover:text-[#1d9bf0]" />
+            </div>
+            {comments.length > 0 && (
+              <span className="group-hover:text-[#1d9bf0] text-sm">
+                {comments.length}
+              </span>
+            )}
+          </div>
+
+          {session.user.uid === post?.id ? (
+            <div
+              className="flex items-center space-x-1 group"
+              onClick={(e) => {
+                e.stopPropagation();
+                deleteDoc(doc(db, "posts", id));
+                router.push("/");
+              }}
+            >
+              <div className="icon group-hover:bg-red-600/10">
+                <TrashIcon className="h-5 group-hover:text-red-600" />
+              </div>
+            </div>
+          ) : (
+            <div className="flex items-center space-x-1 group">
+              <div className="icon group-hover:bg-green-500/10">
+                <SwitchHorizontalIcon className="h-5 group-hover:text-green-500" />
+              </div>
+            </div>
+          )}
+
+          <div
+            className="flex items-center space-x-1 group"
+            onClick={(e) => {
+              e.stopPropagation();
+              likePost();
+            }}
+          >
+            <div className="icon group-hover:bg-pink-600/10">
+              {liked ? (
+                <HeartIconFilled className="h-5 text-pink-600" />
+              ) : (
+                <HeartIcon className="h-5 group-hover:text-pink-600" />
+              )}
+            </div>
+            {likes.length > 0 && (
+              <span
+                className={`group-hover:text-pink-600 text-sm ${
+                  liked && "text-pink-600"
+                }`}
+              >
+                {likes.length}
+              </span>
+            )}
+          </div>
+
+          <div className="icon group">
+            <ShareIcon className="h-5 group-hover:text-[#1d9bf0]" />
+          </div>
+          <div className="icon group">
+            <ChartBarIcon className="h-5 group-hover:text-[#1d9bf0]" />
+          </div>
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default Post;
