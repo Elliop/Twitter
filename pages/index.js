2218bff80596547a4bcfb02b88c5c06ee34249ef
Elliop <abdelmouhayminegx@gmail.com>
2021-12-28 18:36:36 +0100

    Add modal and comment fnct

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,13 @@ import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 import { getProviders, getSession, useSession } from "next-auth/react";
 import Login from "../components/Login";
+import Modal from "../components/Modal";
+import { modalState } from "../atoms/modalAtom";
+import { useRecoilState } from "recoil";
 
 export default function Home({ providers }) {
   const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useRecoilState(modalState);
 
   if (!session) return <Login providers={providers} />;
 
@@ -19,6 +23,7 @@ export default function Home({ providers }) {
         <Sidebar />
         <Feed />
         {/* Widgets */}
+        {isOpen && <Modal />}
       </main>
     </div>
   );
