c5cbe2813e8a0d2416239c5ae76522c54e1a803f
Elliop <abdelmouhayminegx@gmail.com>
2021-12-28 17:49:19 +0100

    Add header of the post

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,8 +1,44 @@
-const Post = () => {
+const Post = ({ id, post, postPage }) => {
   return (
-    <div>
-      {/*  */}
-      post
+    <div className="p-3 flex cursor-pointer border-b border-gray-700">
+      {!postPage && (
+        <img
+          src={post?.userImg}
+          alt=""
+          className="h-11 w-11 rounded-full mr-4"
+        />
+      )}
+      <div className="flex flex-col space-y-2 w-full">
+        <div className={`flex ${!postPage && "justify-between"}`}>
+          {postPage && (
+            <img
+              src={post?.userImg}
+              alt="Profile Pic"
+              className="h-11 w-11 rounded-full mr-4"
+            />
+          )}
+          <div className="text-[#6e767d]">
+            <div className="inline-block group">
+              <h4
+                className={`font-bold text-[15px] sm:text-base text-[#d9d9d9] group-hover:underline ${
+                  !postPage && "inline-block"
+                }`}
+              >
+                {post?.username}
+              </h4>
+              <span
+                className={`text-sm sm:text-[15px] ${!postPage && "ml-1.5"}`}
+              >
+                @{post?.tag}
+              </span>
+            </div>{" "}
+            ·{" "}
+            <span className="hover:underline text-sm sm:text-[15px]">
+              {/* <Moment fromNow>{post?.timestamp?.toDate()}</Moment> */}
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
