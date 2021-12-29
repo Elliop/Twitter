1dfc4ad7acef27fc5b14fcd7d4ed9b9a95a871c3
Elliop <abdelmouhayminegx@gmail.com>
2021-12-29 15:52:10 +0100

    Fix eact/no-unescaped-entities in Widgets

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -61,7 +61,7 @@ const Widgets = () => {
         </div>
       </div>
       <div className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
-        <h4 className="font-bold text-xl px-4">What's happening</h4>
+        <h4 className="font-bold text-xl px-4">{`What's happening`}</h4>
         {trendingData.map((result, index) => (
           <Trending key={index} result={result} />
         ))}
