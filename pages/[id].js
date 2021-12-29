2dc132bae6d4db02be340eb42d6d9c89d0a647e3
Elliop <abdelmouhayminegx@gmail.com>
2021-12-29 13:24:41 +0100

    Add Header on PostPage

--- pages/[id].js
+++ pages/[id].js
@@ -45,6 +45,20 @@ const PostPage = ({ providers }) => {
       </Head>
       <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
         <Sidebar />
+        <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
+          <div
+            className="flex items-center px-1.5 py-2 border-b border-gray-700 text-[#d9d9d9] 
+          font-semibold text-xl gap-x-4 sticky top-0 z-50 bg-black"
+          >
+            <div
+              className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0"
+              onClick={() => router.push("/")}
+            >
+              <ArrowLeftIcon className="h-5 text-white" />
+            </div>
+            Tweet
+          </div>
+        </div>
         {isOpen && <Modal />}
       </main>
     </div>
