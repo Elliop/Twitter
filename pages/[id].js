faed5948d37c64fcaa19d5b225eff4e54ea5137d
Elliop <abdelmouhayminegx@gmail.com>
2021-12-29 13:21:33 +0100

    Add custom Head in PostPage

--- pages/[id].js
+++ pages/[id].js
@@ -1,10 +1,66 @@
-const PostPage = () => {
+import {
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+} from "@firebase/firestore";
+import { getProviders, getSession, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { modalState } from "../atoms/modalAtom";
+import Modal from "../components/Modal";
+import Sidebar from "../components/Sidebar";
+import Post from "../components/Post";
+import { db } from "../firebase";
+import { ArrowLeftIcon } from "@heroicons/react/solid";
+import Head from "next/head";
+import Login from "../components/Login";
+
+const PostPage = ({ providers }) => {
+  const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useRecoilState(modalState);
+  const router = useRouter();
+  const { id } = router.query;
+  const [post, setPost] = useState();
+
+  useEffect(
+    () =>
+      onSnapshot(doc(db, "posts", id), (snapshot) => {
+        setPost(snapshot.data());
+      }),
+    [db]
+  );
+
+  if (!session) return <Login providers={providers} />;
+
   return (
     <div>
-      {/*  */}
-      {/*  */}
+      <Head>
+        <title>
+          {post?.username} on Twitter: "{post?.text}"
+        </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
+        <Sidebar />
+        {isOpen && <Modal />}
+      </main>
     </div>
   );
 };
 
 export default PostPage;
+
+export async function getServerSideProps(context) {
+  const providers = await getProviders();
+  const session = await getSession(context);
+
+  return {
+    props: {
+      providers,
+      session,
+    },
+  };
+}
