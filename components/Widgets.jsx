7f19207ad00d75b08ec3ceebc6cd40cd7a0963db
Elliop <abdelmouhayminegx@gmail.com>
2021-12-29 13:54:39 +0100

    Add Search Bar

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -1,8 +1,20 @@
+import { SearchIcon } from "@heroicons/react/outline";
+
 const Widgets = () => {
   return (
-    <div>
-      {/*  */}
-      Widgets
+    <div className="hidden lg:inline ml-8 xl:w-[450px] py-1 space-y-5">
+      <div className="sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12">
+        <div className="flex items-center bg-[#202327] p-3 rounded-full relative">
+          <SearchIcon className="text-gray-500 h-5 z-50" />
+          <input
+            type="text"
+            className="bg-transparent placeholder-gray-500 outline-none text-[#d9d9d9] 
+            absolute inset-0 pl-11 border border-transparent w-full focus:border-[#1d9bf0] rounded-full 
+            focus:bg-black focus:shadow-lg"
+            placeholder="Search Twitter"
+          />
+        </div>
+      </div>
     </div>
   );
 };
